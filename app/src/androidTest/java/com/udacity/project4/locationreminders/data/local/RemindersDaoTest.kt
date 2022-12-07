b5ed253f4466a35230cbb3a9d94e20201af7ec57
Amalia Oktaviana <amalia.okv01@gmail.com>
2022-12-07 20:30:38 +0100

    adding a test about insert a reminder and then get the reminder by its id

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersDaoTest.kt
@@ -5,12 +5,14 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.SmallTest;
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi;
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.runBlockingTest
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.notNullValue
@@ -42,5 +44,20 @@ class RemindersDaoTest {
 
     @After
     fun closeDb() = remindersDatabase.close()
+
+    @Test
+    fun insertReminderAndGetById() = runBlocking {
+        val reminder = ReminderDTO(
+            "Title todo",
+            "Description todo",
+            "Location todo",
+            10.0,
+            20.0)
+
+        remindersDatabase.reminderDao().saveReminder(reminder)
+        val result: ReminderDTO? = remindersDatabase.reminderDao().getReminderById(reminder.id)
+
+        assertThat(result, `is`(reminder))
+    }
 
 }
