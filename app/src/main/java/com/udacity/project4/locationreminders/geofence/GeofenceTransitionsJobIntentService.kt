2cd7d6219d8f8d9fb88b7ef365671c714be427f0
Amalia Oktaviana <amalia.okv01@gmail.com>
2022-12-07 18:13:54 +0100

    adding implementation in onHandleWork to handle the geofencing transition events and send notification when user enters the geofence area

--- app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/geofence/GeofenceTransitionsJobIntentService.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.JobIntentService
 import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofencingEvent
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 import com.udacity.project4.locationreminders.data.ReminderDataSource
@@ -34,9 +35,8 @@ class GeofenceTransitionsJobIntentService : JobIntentService(), CoroutineScope {
     }
 
     override fun onHandleWork(intent: Intent) {
-        //TODO: handle the geofencing transition events and
-        // send a notification to the user when he enters the geofence area
-        //TODO call @sendNotification
+        val geofencingEvent = GeofencingEvent.fromIntent(intent)
+        sendNotification(geofencingEvent.triggeringGeofences)
     }
 
     //TODO: get the request id of the current geofence
